0536b1b356187f35001a37eddbd171a2e964a3eb
Akiyama Kaito <s22c1002vv@s.chibakoudai.jpgit config  --global user.email s22c1002vv@s.chibakoudai.jpgit config  --global user.email s22c1002vv@s.chibakoudai.jp>
2023-11-16 12:34:55 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Kaito Akiyama
+# SPDX-License-Identifier: BSD-3-Claunse
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
 
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 14 ]
+[ "$res" = 0 ] && echo OK
+exit $res
